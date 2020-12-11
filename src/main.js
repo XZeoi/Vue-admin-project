ff77735ad71135deb5a42c1bd7fd94114cb9be5c
Zoei <taojun1991@126.com>
2020-12-11 22:39:42 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+//导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = `https://www.liulongbin.top:8888/api/private/v1/`
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,15 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+import { Input } from 'element-ui'
+import { Form, FormItem} from 'element-ui'
+//导入弹框提示组件
+import { Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+//与element-UI的其它组件不同（这里不用vue的use方法）
+//把弹框组件挂载到Vue的原型上，这样每个组件都可以用到
+Vue.prototype.$message = Message
